43c8bfb59582a5678a727d8503dcc474683f6506
chajeehyung <chajee308@gmail.com>
2018-09-27 16:56:53 +0900

    r'^$'패턴과 request 요청 실습

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.utils import timezone
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    current_time = timezone.now()
+
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post list</h1>'
+        '<p>{}</p>'
+        '</body>'
+        '</html>'.format(
+            current_time.strftime('%y. %m. %d<br>%H:%M:%S')
+        )
+
+    )
