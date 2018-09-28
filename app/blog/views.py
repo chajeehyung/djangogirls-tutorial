7dcdfe1ab0d26f90378656624daee711cfe33c99
chajeehyung <chajee308@gmail.com>
2018-09-28 11:38:09 +0900

    render()에서 shortcut함수 사용

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,6 @@
+import random
+
+from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
@@ -5,7 +8,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def post_list(request):
-    templates = loader.get_template('blog/post_list.html')
-    context = {}
-    content = templates.render(context, request)
-    return HttpResponse(content)
+    # templates = loader.get_template('blog/post_list.html')
+    # context = {
+    #     'pokemon' : random.choice('피카츄','꼬부기')
+    # }
+    # content = templates.render(context, request)
+    # return HttpResponse(content)
+
+    context = {
+        'pokemon': random.choice('피카츄', '꼬부기')
+    }
+
+    return render(
+        request = request,
+        template_name = 'blog/post_list.html',
+        context = context,
+    )
